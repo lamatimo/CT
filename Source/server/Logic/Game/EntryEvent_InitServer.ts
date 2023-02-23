8a4d7d2953a95b3c1ebc60ec309a09c96c3b0a8f
lamatimo <42297620+lamatimo@users.noreply.github.com>
2023-02-23 19:07:05 +0800

    添加protobufjs

--- Source/server/Logic/Game/EntryEvent_InitServer.ts
+++ Source/server/Logic/Game/EntryEvent_InitServer.ts
@@ -4,9 +4,46 @@ import { AEvent } from "../../../client/assets/Scripts/Core/EventSystem/AEvent";
 import { EventDecorator } from "../../../client/assets/Scripts/Core/EventSystem/EventDecorator";
 import { ctLog } from "../../../client/assets/Scripts/Core/Log/Logger";
 
+import { Field, Message, OneOf, Type } from "protobufjs"
+
+export class AwesomeSubMessage extends Message<AwesomeSubMessage> {
+
+    @Field.d(1, "string")
+    public awesomeString: string;
+
+}
+
+export enum AwesomeEnum {
+    ONE = 1,
+    TWO = 2
+}
+
+@Type.d("SuperAwesomeMessage")
+export class AwesomeMessage extends Message<AwesomeMessage> {
+
+    @Field.d(1, "string", "optional", "awesome default string")
+    public awesomeField: string;
+
+    @Field.d(2, AwesomeSubMessage)
+    public awesomeSubMessage: AwesomeSubMessage;
+
+    @Field.d(3, AwesomeEnum, "optional", AwesomeEnum.ONE)
+    public awesomeEnum: AwesomeEnum;
+
+    @OneOf.d("awesomeSubMessage", "awesomeEnum")
+    public which: string;
+
+}
+
 @EventDecorator(EntryEvent)
 class EntryEvent_InitServer extends AEvent<EntryEvent>{
     protected async run(scene: Scene, args: EntryEvent) {
-        ctLog("事件收到")
+
+        let message = new AwesomeMessage({ awesomeField: "hello" });
+        let buffer = AwesomeMessage.encode(message).finish();
+        let decoded = AwesomeMessage.decode(buffer);
+
+        ctLog(buffer)
+        ctLog(decoded)
     }
 }
